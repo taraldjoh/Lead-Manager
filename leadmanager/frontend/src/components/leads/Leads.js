f160045f2b4840be1ba4632035506744e58138ab
Tarald Johansen <tarald.johansen@outlook.com>
2019-05-16 16:51:12 +0200

    get leads

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
 
-export default class Leads extends Component {
+export class Leads extends Component {
+  static propTypes = {
+    leads: PropTypes.array.isRequired
+  };
   render() {
     return (
       <div>
         <h1>Leads List</h1>
       </div>
-    )
+    );
   }
 }
+
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(mapStateToProps)(Leads);
