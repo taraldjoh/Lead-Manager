f14692afb7bed9c6fef5cb62b60c36680ef51c08
Tarald Johansen <tarald.johansen@outlook.com>
2019-05-18 16:15:17 +0200

    format

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -4,9 +4,7 @@ import { GET_LEADS } from "./types";
 
 // Get Leads ACtion method
 export const getLeads = () => dispatch => {
-  axios
-    .get("/api/leads/")
-    .then(res => {
+  axios.get("/api/leads/").then(res => {
       dispatch({
         type: GET_LEADS,
         payload: res.data
