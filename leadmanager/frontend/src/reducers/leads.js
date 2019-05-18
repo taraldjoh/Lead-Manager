aa40e078c9e4eda6a81d38a50afd9b51965fde12
Tarald Johansen <tarald.johansen@outlook.com>
2019-05-18 16:15:00 +0200

    typo

--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -4,7 +4,7 @@ const initialState = {
   leads: []
 };
 
-export default function(state = initialState, actions) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case GET_LEADS:
       return {
