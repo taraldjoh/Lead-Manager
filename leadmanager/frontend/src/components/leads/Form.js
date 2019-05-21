d52b0541748800a21bcca014ad7eb72655b2c867
Tarald Johansen <tarald.johansen@outlook.com>
2019-05-21 16:20:06 +0200

    added form

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: '',
+  }
   render() {
     return (
-      <div>
-        <h1 className="p-4">Add Lead Form</h1>
+      <div className="card card-body mt-4 mb-4">
+        <h2>Add Lead</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              onChange={this.onChange}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              onChange={this.onChange}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <label>Message</label>
+            <textarea
+              className="form-control"
+              type="text"
+              name="message"
+              onChange={this.onChange}
+              value={message}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     )
   }
