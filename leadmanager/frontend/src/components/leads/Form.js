071d820a4512e6675e49c3cbe2e51fc48f99fe05
Tarald Johansen <tarald.johansen@outlook.com>
2019-05-23 17:21:51 +0200

    add onchange and onsubmit events

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types'
+import { addLead } from '../../actions/leads'
 
-export default class Form extends Component {
+export class Form extends Component {
   state = {
     name: '',
     email: '',
     message: '',
   }
+  static propTypes = {
+    addLead: PropTypes.func.isRequired
+  }
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { name , email, message } = this.state;
+    const lead = { name, email, message }
+    this.props.addLead(lead);
+  }
+
   render() {
+    const {name, email, message} = this.state;
     return (
       <div className="card card-body mt-4 mb-4">
         <h2>Add Lead</h2>
@@ -51,3 +68,5 @@ export default class Form extends Component {
     )
   }
 }
+
+export default connect(null, { addLead })(Form);
