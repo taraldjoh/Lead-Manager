ba18b77a3e9c0ee824c6c5e1b40d9d3bfcbc2d75
Tarald Johansen <tarald.johansen@outlook.com>
2019-05-23 22:26:42 +0200

    add lead action

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // Get Leads ACtion method
 export const getLeads = () => dispatch => {
@@ -19,6 +19,17 @@ export const deleteLead = (id) => dispatch => {
       dispatch({
         type: DELETE_LEAD,
         payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// Add Lead
+export const addLead = (lead) => dispatch => {
+  axios.post("/api/leads/", lead).then(res => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
       });
     })
     .catch(err => console.log(err));
